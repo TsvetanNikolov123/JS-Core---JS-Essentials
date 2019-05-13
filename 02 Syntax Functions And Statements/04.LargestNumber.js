16832814c4c531a498e34a38aced972ace5aece1
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-05-13 23:37:29 +0300

    solution - 04. Largest Number

---
+++ 02 Syntax Functions And Statements/04.LargestNumber.js
@@ -0,0 +1,8 @@
+function solution(num1, num2, num3) {
+    let biggestNumber = Math.max(num1, num2, num3);
+
+    console.log(`The largest number is ${biggestNumber}.`);
+}
+
+solution(5, -3, 16);
+solution(-3, -5, -22.5);
