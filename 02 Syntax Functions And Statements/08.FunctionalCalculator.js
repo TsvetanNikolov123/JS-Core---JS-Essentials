4f2e27bc67a12fa35be588b5abeb0c5595652899
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-05-14 15:00:33 +0300

    solution - 08. Functional Calculator

---
+++ 02 Syntax Functions And Statements/08.FunctionalCalculator.js
@@ -0,0 +1,36 @@
+function calculate(a, b, operator) {
+    let calc = function (a, b, operator) {
+        return operator(a, b);
+    };
+
+    let add = function (a, b) {
+        return a + b;
+    };
+
+    let subtract = function (a, b) {
+        return a - b;
+    };
+
+    let multiply = function (a, b) {
+        return a * b;
+    };
+
+    let divide = function (a, b) {
+        return a / b;
+    };
+
+    switch (operator) {
+        case '+':
+            return calc(a, b, add);
+        case '-':
+            return calc(a, b, subtract);
+        case '*':
+            return calc(a, b, multiply);
+        case '/':
+            return calc(a, b, divide);
+    }
+}
+
+console.log(calculate(2, 4, '+'));
+console.log(calculate(3, 3, '/'));
+console.log(calculate(18, -1, '*'));
