8ff434d02744f5ded66fc7484cda728ff742f974
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-05-15 23:47:20 +0300

    solution - 06. Road Radar

---
+++ 04 Exercise Syntax Functions And Statements/06.RoadRadar.js
@@ -0,0 +1,43 @@
+function solution([inputSpeed, inputZone]) {
+    let speed = inputSpeed;
+    let zone = inputZone;
+    let limit = getLimit(zone);
+    let infraction = getInfraction(speed, limit);
+
+    if (infraction) {
+        console.log(infraction);
+    }
+
+    function getLimit(zone) {
+        switch (zone) {
+            case 'motorway':
+                return 130;
+            case 'interstate':
+                return 90;
+            case 'city':
+                return 50;
+            case 'residential':
+                return 20;
+        }
+    }
+
+    function getInfraction(speed, limit) {
+        let overSpeed = speed - limit;
+        if (overSpeed <= 0) {
+            return false
+        } else {
+            if (overSpeed <= 20) {
+                return 'speeding';
+            } else if (overSpeed <= 40) {
+                return 'excessive speeding';
+            } else {
+                return 'reckless driving';
+            }
+        }
+    }
+}
+
+solution([40, 'city']);
+solution([21, 'residential']);
+solution([120, 'interstate']);
+solution([200, 'motorway']);
