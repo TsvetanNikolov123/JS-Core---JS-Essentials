8912b467b2e0d49442935476ba1a5b2e939b3516
TsvetanNikolov123 <tsdman1985@gmail.com>
2019-06-04 12:48:02 +0300

    solution - 01. Courses Prices

---
+++ 12 Exam Preparation/01. Courses Prices/01.CoursesPrices.js
@@ -0,0 +1,37 @@
+function solution(fundamentals, advanced, apps, form) {
+    const courses = {
+        'JS Fundamentals': 170,
+        'JS Advanced': 180,
+        'JS Apps': 190
+    };
+
+    let total = 0;
+
+    if (fundamentals) {
+        total += courses['JS Fundamentals'];
+    }
+
+    if (advanced) {
+        total += courses['JS Advanced'];
+    }
+
+    if (apps) {
+        total += courses['JS Apps'];
+    }
+
+    if (fundamentals && advanced) {
+        total -= courses['JS Advanced'] * 0.1;
+    }
+
+    if (fundamentals && advanced && apps) {
+        total -= total * 0.06;
+    }
+
+    if (form === 'online') {
+        total -= total * 0.06;
+    }
+
+    console.log(Math.round(total));
+}
+
+solution(true, false, false, "onsite");
